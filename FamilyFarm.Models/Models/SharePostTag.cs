a3fb94328f867b0cd5a0d4ef2836122d25ec3693
KhoaKiro <kdang6609@gmail.com>
2025-04-19 17:31:58 +0700

    Add khoa's model

---
+++ FamilyFarm.Models/Models/SharePostTag.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace FamilyFarm.Models.Models
+{
+    class SharePostTag
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public required ObjectId SharePostTagId { get; set; }
+        public required ObjectId SharePostId { get; set; }
+        public required ObjectId AccId { get; set; }
+        public required DateTime CreatedAt { get; set; }
+    }
+}
