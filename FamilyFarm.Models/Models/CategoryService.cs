6e6307bf2dddd7f3f77c92a58120dab0411637af
THUC <nht.071203@gmail.com>
2025-04-19 12:56:55 +0700

    add models GroupMember, Notification, CategoryService, ProcessStep, Service, GroupRole

---
+++ FamilyFarm.Models/Models/CategoryService.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyFarm.Models.Models
+{
+    public class CategoryService
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public required ObjectId CategoryServiceId { get; set; }
+        [BsonRequired]
+        public required ObjectId AccId { get; set; }
+        public required string CategoryName { get; set; }
+        public required string CategoryDescription { get; set; }
+        public DateTime? CreateAt { get; set; }
+        public DateTime? UpdateAt { get; set; }
+        public bool? IsDeleted { get; set; }
+    }
+}
