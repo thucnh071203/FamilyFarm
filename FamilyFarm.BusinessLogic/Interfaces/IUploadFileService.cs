06512dc68fb62548dda1ebf8914f0884097e2ee6
tranphuongnamstudy <namtpce170126@fpt.edu.vn>
2025-04-22 20:19:36 +0700

    Add file

---
+++ FamilyFarm.BusinessLogic/Interfaces/IUploadFileService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace FamilyFarm.BusinessLogic.Interfaces
+{
+    interface IUploadFileService
+    {
+        Task<string> UploadImage(IFormFile fileImage);
+    }
+}
---
+++ FamilyFarm.BusinessLogic/Services/UploadFileService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FamilyFarm.BusinessLogic.Config;
+using FamilyFarm.BusinessLogic.Interfaces;
+using Firebase.Storage;
+using Microsoft.AspNetCore.Http;
+
+namespace FamilyFarm.BusinessLogic.Services
+{
+    public class UploadFileService : FirebaseConnection, IUploadFileService
+    {
+        //Upload image to folder image on Firebase Storage
+        public async Task<string> UploadImage(IFormFile fileImage)
+        {
+            var stream = fileImage.OpenReadStream();
+            var fileName = $"image/{DateTime.UtcNow.Ticks}_{fileImage.FileName}";
+
+
+            var storage = new FirebaseStorage(
+            "prn221-69738.appspot.com",
+            new FirebaseStorageOptions
+            {
+                AuthTokenAsyncFactory = () => Task.FromResult(""),
+                ThrowOnCancel = true
+            });
+
+            // Upload file lên Firebase Storage và lấy URL
+            var imageUrl = await storage
+                .Child(fileName)
+                .PutAsync(stream);
+
+            return imageUrl;
+        }
+    }
+}
