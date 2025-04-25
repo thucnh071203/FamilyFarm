51be08a098caec1d2718dada7395d4d3caf980d2
THUC <nht.071203@gmail.com>
2025-04-25 16:03:42 +0700

    add report/share post models, CRUD report

---
+++ FamilyFarm.BusinessLogic/Interfaces/IReportService.cs
@@ -0,0 +1,20 @@
+﻿using FamilyFarm.DataAccess.DAOs;
+using FamilyFarm.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyFarm.BusinessLogic.Interfaces
+{
+    public interface IReportService
+    {
+        Task<List<Report>> GetAll();
+        Task<Report?> GetById(string id);
+        Task<Report?> GetByPostAndReporter(string postId, string reporterId);
+        Task<Report> Create(Report report);
+        Task<Report> Update(string id, Report report);
+        Task Delete(string id);
+    }
+}
