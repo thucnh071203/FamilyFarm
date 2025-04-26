28ed29dcb6a155b08d3f905c5a7f129c494616aa
tranphuongnamstudy <namtpce170126@fpt.edu.vn>
2025-04-26 18:06:51 +0700

    Add files

---
+++ FamilyFarm.Models/DTOs/Request/CreatePostRequestDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace FamilyFarm.Models.DTOs.Request
+{
+    public class CreatePostRequestDTO
+    {
+        public string? PostContent { get; set; }
+        public List<string>? Hashtags { get; set; }
+        public List<string>? ListCategoryOfPost { get; set; }
+        public List<string>? ListTagFriend { get; set; }
+        public List<IFormFile>? ListImage { get; set; }
+        public string? Privacy {  get; set; }
+
+    }
+}
---
+++ FamilyFarm.Models/DTOs/Response/CreatePostResponseDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyFarm.Models.DTOs.Response
+{
+    public class CreatePostResponseDTO
+    {
+        public string? Message { get; set; }
+        public DateTime? CreatedAt { get; set; }
+    }
+}
