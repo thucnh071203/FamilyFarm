b4e3631a07ecbeeffe8a33d33de8442ba84d0b12
KhoaKiro <kdang6609@gmail.com>
2025-04-28 10:48:21 +0700

    Add update profile, optimized login facebook, change query model with role group function

---
+++ FamilyFarm.Models/DTOs/Request/UpdateProfileRequestDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyFarm.Models.DTOs.Request
+{
+    public class UpdateProfileRequestDTO
+    {
+        public required string FullName { get; set; }
+        public DateTime? Birthday { get; set; }
+        public string? Gender { get; set; }
+        public required string City { get; set; }
+        public required string Country { get; set; }
+        public string? Address { get; set; }
+        public string? Background { get; set; }
+
+        // Expert
+        public string? Certificate { get; set; }
+        public string? WorkAt { get; set; }
+        public string? StudyAt { get; set; }
+    }
+}
