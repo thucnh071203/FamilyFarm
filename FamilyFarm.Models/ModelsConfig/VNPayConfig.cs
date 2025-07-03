99f7edc3ee603abf925f4d45cd1d4d3599332075
Dang Nguyen Dang Khoa <khoadndce172075@fpt.edu.vn>
2025-07-03 20:56:57 +0700

    add payment api #1

---
+++ FamilyFarm.Models/ModelsConfig/VNPayConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyFarm.Models.ModelsConfig
+{
+    public class VNPayConfig
+    {
+        public string? TmnCode { get; set; }
+        public string? HashSecret { get; set; }
+        public string? PaymentUrl { get; set; }
+        public string? ReturnUrl { get; set; }
+    }
+}
