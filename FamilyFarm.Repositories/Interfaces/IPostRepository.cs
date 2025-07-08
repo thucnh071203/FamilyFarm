79468632dba392a8157a6d04e202cce786faebfa
THUC <nht.071203@gmail.com>
2025-07-08 19:05:16 +0700

    add api infinite post with share

--- FamilyFarm.Repositories/Interfaces/IPostRepository.cs
+++ FamilyFarm.Repositories/Interfaces/IPostRepository.cs
@@ -1,4 +1,5 @@
-﻿using FamilyFarm.Models.DTOs.Response;
+﻿using FamilyFarm.Models.DTOs.EntityDTO;
+using FamilyFarm.Models.DTOs.Response;
 using FamilyFarm.Models.Models;
 using System;
 using System.Collections.Generic;
@@ -24,6 +25,7 @@ namespace FamilyFarm.Repositories.Interfaces
         Task<SearchPostInGroupResponseDTO> SearchPostsWithAccountAsync(string groupId, string keyword);
         Task<List<Post>?> GetListPost(int is_deleted);
         Task<(List<Post> posts, bool hasMore)> GetPaginatedPosts(string? last_post_id, int page_size);
+        Task<(List<BasePostItem> items, bool hasMore)> GetPaginatedPostsAndSharePosts(string? lastPostId, string? lastSharePostId, int pageSize);
         Task<List<Post>?> GetListPostCheckedByAI();
         Task<List<Post>?> GetListPostByAccId(string? accId, string? privacy);
         Task<(List<Post>, bool)> GetListPostInYourGroup(string? lastPostId, int pageSize, List<string> groupIds);
